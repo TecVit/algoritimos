33a61a7827be37525d01cf723e63a03ed97d5566
TecVit - FMCopling <vitorfreelancer021@gmail.com>
2025-09-17 10:52:59 -0300

    update: bfs'

--- bfs/mina.py
+++ bfs/mina.py
@@ -1,14 +1,39 @@
+from collections import deque
+
+def mina(N, matriz):
+  dist = [[float('inf')] * N for _ in range(N)]
+  dist[0][0] = 0
+  
+  dq = deque()
+  dq.append((0, 0))
+  
+  dx = [0, 0, 1, -1]
+  dy = [1, -1, 0, 0]
+
+  while dq:
+    x, y = dq.popleft()
+
+    for i in range(4):
+      nx, ny = x + dx[i], y + dy[i]
+      if 0 <= nx < N and 0 <= ny < N:
+        custo = matriz[nx][ny]
+        if dist[x][y] + custo < dist[nx][ny]:
+          dist[nx][ny] = dist[x][y] + custo
+          if custo == 0:
+            dq.appendleft((nx, ny))
+          else:
+            dq.append((nx, ny))
+
+  return dist[N-1][N-1]
+
 def main():
   n = int(input())
-  m = [[-1] * (n + 2)]
+  m = []
 
-  for i in range(n):
-    m.append([-1] + list(map(int, input().split())) + [-1])
+  for _ in range(n):
+    m.append(list(map(int, input().split())))
 
-  m.append([-1] * (n + 2))
-
-  for r in m:
-    print(r)
+  print(mina(n, m))
 
   return 0
 
